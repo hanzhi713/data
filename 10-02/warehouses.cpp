882021d78a87e69a493f8d0446b13db39c54ec23
hanzhi713 <hanzhi713@163.com>
2019-10-03 20:37:00 -0400

    tweak warehouses

--- 10-02/warehouses.cpp
+++ 10-02/warehouses.cpp
@@ -1,41 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct item {
-    string name;
-    int count;
-};
-
 int main() {
     int T;
     cin >> T;
-
     for (int i = 0; i < T; ++i) {
         unordered_map<string, int> count;
         int N;
-        cin >>N;
+        cin >> N;
         for (int j = 0; j < N; ++j) {
             string name;
             int num;
             cin >> name >> num;
             count[name] += num;
         }
-        vector<item> all;
         cout << count.size() << endl;
-        for (auto &it: count) {
-            all.push_back({it.first, it.second});
-        }
-
-        sort(all.begin(), all.end(), [](item& a, item&b) {
-            if (a.count == b.count) {
-                return a.name < b.name;
+        vector<pair<string, int>> all(count.begin(), count.end());
+        sort(all.begin(), all.end(), [](auto &a, auto &b) {
+            if (a.second == b.second) {
+                return a.first < b.first;
             } else {
-                return a.count > b.count;
+                return a.second > b.second;
             }
         });
 
         for (auto &t: all) {
-            cout << t.name << " " << t.count << endl;
+            cout << t.first << " " << t.second << endl;
         }
     }
 }
